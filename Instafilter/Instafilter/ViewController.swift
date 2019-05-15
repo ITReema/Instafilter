113043122fa540339283a86ff366dfae9203d94f
Reema <it.reema7@gmail.com>
2019-05-15 18:47:20 +0300

    Add files via upload

---
+++ Instafilter/Instafilter/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  Instafilter
+//
+//  Created by mac_os on 10/09/1440 AH.
+//  Copyright © 1440 mac_os. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var imageView: UIImageView!
+    
+    @IBOutlet var intensity: UISlider!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+    @IBAction func changeFilter(_ sender: Any) {
+    }
+    
+    @IBAction func save(_ sender: Any) {
+    }
+    
+    @IBAction func intensityChnge(_ sender: Any) {
+    }
+    
+}
+
